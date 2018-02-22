f368b08e5afa4bb657d83ccc66b668a26d0b69c0
ZhangMenghe <zmh.leaves@gmail.com>
2018-02-21 17:53:05 -0800

    heightmap generator-p1

--- Source/AutoLayout/heightMapGenerator.cpp
+++ Source/AutoLayout/heightMapGenerator.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "heightMapGenerator.h"
-
+#include <fstream>
+using namespace std;
 
 // Sets default values
 AheightMapGenerator::AheightMapGenerator()
@@ -23,5 +24,28 @@ void AheightMapGenerator::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+void AheightMapGenerator::parser_heightDataFile(const char*filename, Mat_<float>& heightMap) {
+	ifstream instream(filename);
+	string str;
+	vector<vector<float>> contents;
+	char  delims[] = " :,\t\n";
+	char * context = nullptr;
+	while (instream && getline(instream, str)) {
+		if (!str.length())
+			continue;
+		int len = str.length();
+		char * charline = new char[len];
+		int r = strcpy_s(charline, len, str.c_str());
+		vector<float> row;
+		char * token = strtok_s(charline, delims, &context);
+		while (token != nullptr) {
+			row.push_back(atof(token));
+			token = strtok_s(nullptr, delims, &context);
+		}
+		contents.push_back(row);
+	}
+	instream.close();
+	heightMap = Mat(contents);	
 }
 
--- Source/AutoLayout/heightMapGenerator.h
+++ Source/AutoLayout/heightMapGenerator.h
@@ -1,20 +1,20 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+#include "include/opencv2/core/core.hpp"
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "heightMapGenerator.generated.h"
-
+using namespace cv;
 UCLASS()
 class AUTOLAYOUT_API AheightMapGenerator : public AActor
 {
 	GENERATED_BODY()
-	
+private:
+	void parser_heightDataFile(const char*filename, Mat_<float>& heightMap);
 public:	
 	// Sets default values for this actor's properties
 	AheightMapGenerator();
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
